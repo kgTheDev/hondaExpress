40d6cf8c9a75b35ec96f2e7af714803f393199aa
Keith M Goodrich <39138244+KeithTheDev@users.noreply.github.com>
2018-09-03 12:19:38 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://honda:honda44@ds127811.mlab.com:27811/honda' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
